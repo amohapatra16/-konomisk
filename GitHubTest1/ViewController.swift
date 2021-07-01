a920a316a04688b480e8987a3cfdf28690c390bf
amohapatra16 <amohapatra21@bfhsla.org>
2021-07-01 16:38:36 -0500

    Initial Commit

---
+++ GitHubTest1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitHubTest1
+//
+//  Created by Esha Patel on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func startGame(_ sender: UIButton) {
+    }
+    
+}
+
